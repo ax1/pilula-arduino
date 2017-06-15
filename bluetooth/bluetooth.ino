b0db5efe2f39fc9feb21d40c43d8f16fced5755c
Angel Rego <angelrego@gmail.com>
2017-06-15 12:18:37 +0200

    bluetooth: translate to spanish

--- bluetooth/bluetooth.ino
+++ bluetooth/bluetooth.ino
@@ -1,32 +1,41 @@
 /**
+   Recibir datos por BLUETOOTH
+   Instalar programa en el móvil para enviar los datos
    https://play.google.com/store/apps/details?id=appinventor.ai_el_profe_garcia.Arduino_Control_Car
-   BTO4 device is a slave device -> only for receiving data
-   PIN (bluetooth) = 1234
+   el módulo BTO4  es un buetooth de tipo esclavo -> sólo recibirá datos
+   PIN (contraseña del bluetooth) = 1234
+
+   Conexiones:
+   - GND a tierra
+   - VCC a 5 voltios (aunque ponga 3.3V no pasa nada porque la radio sólo la vamos a usar como receptor)
+   - RXD al pin de transmision que configuremos en arduino (no es necesario conectarlo para éste ejercicio)
+   - TXD al pin de recepción que configuremos en arduino
+
 */
 
 #include <SoftwareSerial.h>
- 
+
 int PIN_RX = 10; // arduino RX = radio TX
 int PIN_TX = 11;// arduino TX = radio RX
 SoftwareSerial softwareSerial(PIN_RX, PIN_TX);
 
 void setup() {
-  Serial.begin(9600); // This is the serial to communicate with the PC
-  softwareSerial.begin(9600); // This is the serial to communicate with the radio
+  Serial.begin(9600);
+  softwareSerial.begin(9600); // Es un serial adicional para poder comunicarnos con la radio
 }
 
 void loop() {
   if (softwareSerial.available()) {
-    int action = softwareSerial.read();
-    Serial.write(action);
-    execute(action);
+    int comando = softwareSerial.read();
+    Serial.write(comando);
+    ejecutar(comando);
   }
 }
 
-void execute(int action) {
-  switch (action) {
+void ejecutar(int comando) {
+  switch (comando) {
     case 'f' :
-      Serial.println( "ON button");
+      Serial.println( "botón ON");
       break;
     default:
       ;
