44a5ddeed63dc14629ac39ef7abecd2606c7f284
rmoneo <raul.moneo@tecnalia.com>
2017-06-27 16:58:10 +0200

    Version con todos los textos incluidos, la presentacion de powerpoint, la practica del LDR y algunas otras  modificaciones.

---
+++ Ejercicio8-Rele-LDR/Ejercicio8-Rele-LDR.ino
@@ -0,0 +1,22 @@
+int RelePin = 13; // Asigna RelePin al pin 13
+int LDRPin = A0; // Asigna LDRPin al pin A0
+int umbral = 100; //este valor lo obtenemos poniendo la mano sobre la LDR 
+
+
+void setup() 
+{
+   pinMode(RelePin, OUTPUT); // Establece un pin digital como salida.  
+   pinMode(LDRPin, INPUT); // Establece un pin analógico como entrada.  
+}
+
+
+ void loop() 
+{
+       if (analogRead(LDRPin)< umbral) { // compara el valor de la LDR con en que hemos asignado en la variable “umbral”
+      digitalWrite(RelePin, HIGH);
+ }
+   else {
+      digitalWrite(RelePin, LOW);
+   }
+}
+
